5d1c2ac4d3cbbbbb6295b359c7f323bec1cfb171
Just-explore <71567088+Just-explore@users.noreply.github.com>
2020-09-27 01:24:20 +0530

    Update main1.go

--- main1.go
+++ main1.go
@@ -1,1 +1,26 @@
 
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello Santhosh",
+	}, nil
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello Santhosh! twice",
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
